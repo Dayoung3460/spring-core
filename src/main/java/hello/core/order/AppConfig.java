83b6d5f2c88337120c5d85087165dcfbdf023de5
Clara <kimdayoung771@gmail.com>
2024-05-21 22:02:00 +0900

    Refactor AppConfig

--- src/main/java/hello/core/order/AppConfig.java
+++ src/main/java/hello/core/order/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core.order;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,12 +10,19 @@ import hello.core.member.MemoryMemberRepository;
 // 실제 동작에 필요한 구현 객체를 여기서 생성 및 연결
 public class AppConfig {
     public MemberService memberService() {
-        // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
