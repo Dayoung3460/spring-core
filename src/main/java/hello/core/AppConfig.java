e9e7d3740173237cff41a71ab2d2bdd8ec07ef43
Clara <kimdayoung771@gmail.com>
2024-06-11 17:05:00 +0900

    Added ConfigurationSingletonTest

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -16,19 +16,26 @@ import org.springframework.context.annotation.Configuration;
 // 실제 동작에 필요한 구현 객체를 여기서 생성 및 연결
 
 public class AppConfig {
+    // memberService() 호출하면서 memberRepository()가 호출되고
+    // orderService()가 호출되면서 memberRepository() 또 호출됨
+    // 동일한 객체가 두개 생성됨. 싱글톤 깨지는 것 처럼 보임
+
     @Bean
     // return 된 객체들이 스프링 컨테이너에 등록됨
     // 메서드 이름 = 스프링 빈 이름
     // 빈 이름은 중복되면 안됨
     public MemberService memberService() {
+        System.out.println("Call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("Call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService() {
+        System.out.println("Call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
