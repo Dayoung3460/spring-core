706a1630cbafa1b49eafb0d41d1a291430e1ba77
dayoung <kimdayoung771@gmail.com>
2024-07-09 15:42:36 +0900

    InitializingBean & DisposableBean

--- src/test/java/hello/core/lifeCycle/NetworkClient.java
+++ src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -1,15 +1,17 @@
 package hello.core.lifeCycle;
 
 import lombok.Setter;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
+// 초기화, 소멸 인터페이스 사용(잘 사용 안함)
 @Setter
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("Calling constructor " + url);
-        connect();
-        call("message text");
+
     }
 
     // Call when service starts
@@ -25,5 +27,20 @@ public class NetworkClient {
     public void disconnect() {
         System.out.println("disconnect: " + url);
 
+    }
+
+    // 의존관계 주입 끝나고 실행
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
+        connect();
+        call("Initializing connect msg");
+    }
+
+    // 빈 종료될 때 호출
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("destroy");
+        disconnect();
     }
 }
